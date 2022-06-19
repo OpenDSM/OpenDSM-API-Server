6abe635ea569f0d60f71648222a0ba7e0a8fd12a
Drew Chase <dcmanproductions@gmail.com>
2022-06-19 18:15:15 -0400

    Add project files.

---
+++ API/Controllers/API/TemplateAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.API
+{
+    [ApiController]
+    [Route("/api/template")]
+    public class TemplateAPIController : ControllerBase
+    {
+        #region Public Methods
+
+        [HttpGet()]
+        public IActionResult Index()
+        {
+            return Ok(new
+            {
+                message = "This is the template",
+                time = DateTime.Now.ToString("MM/dd/yyyy - HH:mm:ss.fff")
+            });
+        }
+
+        #endregion Public Methods
+    }
+}
