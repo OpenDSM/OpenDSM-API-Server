5279fb16c086eb2a35a4c3d431c013147af133a4
Drew Chase <dcmanproductions@gmail.com>
2022-06-19 19:15:11 -0400

    Added Home

---
+++ API/Controllers/VIEWS/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.VIEWS;
+
+[Route("/")]
+public class HomeController : Controller
+{
+    #region Public Methods
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/product/{Owner}/{Slug}")]
+    public IActionResult Product(string Owner, string Slug)
+    {
+        ViewBag.Owner = Owner;
+        ViewBag.Slug = Slug;
+        return View();
+    }
+
+    #endregion Public Methods
+}
