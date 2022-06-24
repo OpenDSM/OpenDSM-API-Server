092ea773f7265b493300540756d504b06cb997d2
Drew Chase <dcmanproductions@gmail.com>
2022-06-24 01:07:31 -0400

    Removed call to open url and initialized Account Management

--- API/Program.cs
+++ API/Program.cs
@@ -1,89 +1,78 @@
-using ChaseLabs.CLLogger;
-using ChaseLabs.CLLogger.Interfaces;
+global using static OpenDSM.Lib.Data.Global;
 using CLPortmapper;
-using System.Diagnostics;
-using System.Reflection;
+using OpenDSM.Lib.Auth;
 
-namespace API
+namespace API;
+
+public class Program
 {
-    public class Program
+    #region Fields
+
+    private static int port = 8080;
+
+    #endregion Fields
+
+    #region Private Methods
+
+    private static void Main(string[] args)
     {
-        #region Fields
-
-        private static ILog log = LogManager.Init().SetDumpMethod(DumpType.NoBuffer).SetLogDirectory(Path.Combine(Assembly.GetExecutingAssembly().Location));
-        private static int port = 8256;
-
-        #endregion Fields
-
-        #region Private Methods
-
-        private static void Main(string[] args)
+        log.Info("Welcome to Kestrel Template");
+        log.Debug("All Rights Reserved - LFInteractive LLC. (c) 2020-2022");
+        if (args.Length == 0)
         {
-            log.Info("Welcome to Kestrel Template");
-            log.Debug("All Rights Reserved - LFInteractive LLC. (c) 2020-2022");
-            if (args.Length == 0)
+            PortHandler.AddToFirewall();
+            log.Warn($"Automatic Port Forwarding is Enabled");
+            log.Debug($"Opening Port {port}");
+            PortHandler.OpenPort(port);
+            _ = AccountManagement.Instance;
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder =>
+            {
+                log.Info($"Starting server on port {port}");
+                builder.UseIISIntegration();
+                builder.UseContentRoot(Directory.GetCurrentDirectory());
+                builder.UseKestrel(options =>
+                {
+                    options.ListenAnyIP(port);
+                });
+                builder.UseStartup<Startup>();
+                log.Info("Server is now running!");
+            }).Build().Run();
+
+            log.Info("Shutting Down");
+            PortHandler.ClosePort(port);
+        }
+        else
+        {
+            if (args[0] == "--firewall")
             {
                 PortHandler.AddToFirewall();
-                log.Warn($"Automatic Port Forwarding is Enabled");
-                log.Debug($"Opening Port {port}");
-                PortHandler.OpenPort(port);
-                Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder =>
-                {
-                    log.Info($"Starting server on port {port}");
-                    builder.UseIISIntegration();
-                    builder.UseContentRoot(Directory.GetCurrentDirectory());
-                    builder.UseKestrel(options =>
-                    {
-                        options.ListenAnyIP(port);
-                    });
-                    builder.UseStartup<Startup>();
-                    log.Info("Server is now running!");
-                    new Process()
-                    {
-                        StartInfo = new()
-                        {
-                            FileName = $"http://127.0.0.1:{port}/api/template",
-                            UseShellExecute = true,
-                        }
-                    }.Start();
-                }).Build().Run();
-
-                log.Info("Shutting Down");
-                PortHandler.ClosePort(port);
-            }
-            else
-            {
-                if (args[0] == "--firewall")
-                {
-                    PortHandler.AddToFirewall();
-                }
             }
         }
-
-        #endregion Private Methods
     }
 
-    internal class Startup
+    #endregion Private Methods
+}
+
+internal class Startup
+{
+    #region Public Methods
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment evn)
     {
-        #region Public Methods
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment evn)
-        {
-            app.UseForwardedHeaders();
-            app.UseMvc();
-            app.UseRouting();
-            app.UseStaticFiles();
-            app.UseDefaultFiles();
-        }
-
-        public void ConfigureServices(IServiceCollection service)
-        {
-            service.AddMvc(action =>
-            {
-                action.EnableEndpointRouting = false;
-            });
-        }
-
-        #endregion Public Methods
+        app.UseForwardedHeaders();
+        app.UseMvc();
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseDefaultFiles();
     }
+
+    public void ConfigureServices(IServiceCollection service)
+    {
+        service.AddMvc(action =>
+        {
+            action.EnableEndpointRouting = false;
+        });
+    }
+
+    #endregion Public Methods
 }
