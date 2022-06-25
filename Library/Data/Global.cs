42556128f99db2977957a760262b1042eb7fbe01
Drew Chase <dcmanproductions@gmail.com>
2022-06-25 16:28:35 -0400

    Created Product Directory, Added Dedicated Startup Procedure

--- Library/Data/Global.cs
+++ Library/Data/Global.cs
@@ -2,6 +2,8 @@
 
 using ChaseLabs.CLLogger;
 using ChaseLabs.CLLogger.Interfaces;
+using OpenDSM.Lib.Auth;
+using OpenDSM.Lib.Objects;
 using System.Reflection;
 
 namespace OpenDSM.Lib.Data;
@@ -17,8 +19,22 @@ public static class Global
     #region Properties
 
     public static string ConfigDirectory => Directory.CreateDirectory(Path.Combine(RootDirectory, "config")).FullName;
+    public static string ProductsDirectory => Directory.CreateDirectory(Path.Combine(RootDirectory, "products")).FullName;
     public static string RootDirectory => Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LFInteractive", "OpenDSM")).FullName;
     public static string UsersDirectory => Directory.CreateDirectory(Path.Combine(RootDirectory, "users")).FullName;
 
     #endregion Properties
+
+    #region Public Methods
+
+    public static Task InitiateStartupProceedure()
+    {
+        return Task.Run(() =>
+        {
+            Products.Instance.Load();
+            AccountManagement.Instance.Load();
+        });
+    }
+
+    #endregion Public Methods
 }
