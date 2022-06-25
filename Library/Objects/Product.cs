1e5d3a3f6d7edcd209cf1e9bde7254d9fb69850f
Drew Chase <dcmanproductions@gmail.com>
2022-06-25 18:25:38 -0400

    Separated Products from Product file and Added Reviews object

--- Library/Objects/Product.cs
+++ Library/Objects/Product.cs
@@ -3,66 +3,6 @@ using OpenDSM.Lib.Auth;
 
 namespace OpenDSM.Lib.Objects;
 
-public class Products
-{
-    #region Fields
-
-    public static Products Instance = Instance ??= new();
-    public Dictionary<long, Product> _products;
-
-    #endregion Fields
-
-    #region Protected Constructors
-
-    protected Products()
-    {
-        _products = new();
-    }
-
-    #endregion Protected Constructors
-
-    #region Public Methods
-
-    public void Create()
-    {
-    }
-
-    public Product[] GetFromUser(User user)
-    {
-        Product[] products = new Product[user.OwnedProducts.Length];
-        bool hadNull = false;
-        for (int i = 0; i < products.Length; i++)
-        {
-            long productID = user.OwnedProducts[i];
-            if (_products.ContainsKey(productID))
-            {
-                products[i] = _products[user.OwnedProducts[i]];
-            }
-            else
-            {
-                products[i] = null;
-                hadNull = true;
-            }
-        }
-        return hadNull ? products.Where(i => i != null).ToArray() : products;
-    }
-
-    public void Load()
-    {
-        string[] files = Directory.GetFiles(ProductsDirectory, "*.json", SearchOption.TopDirectoryOnly);
-        Parallel.ForEach(files, file =>
-        {
-            FileInfo fileInfo = new(file);
-            if (int.TryParse(fileInfo.Name.Replace(fileInfo.Extension, ""), out int id))
-            {
-                _products.Add(id, new(id));
-            }
-        });
-    }
-
-    #endregion Public Methods
-}
-
 public class Product
 {
     #region Fields
@@ -101,19 +41,33 @@ public class Product
     #region Properties
 
     public string Banner { get => manager.GetConfigByKey("banner_url").Value; set => manager.GetConfigByKey("banner_url").Value = value; }
+
     public string Description { get => manager.GetConfigByKey("description").Value; set => manager.GetConfigByKey("description").Value = value; }
 
     public string Icon { get => manager.GetConfigByKey("icon_url").Value; set => manager.GetConfigByKey("icon_url").Value = value; }
+
     public long ID { get => manager.GetConfigByKey("id").Value; set => manager.GetConfigByKey("id").Value = value; }
+
     public bool IsExperimental { get => manager.GetConfigByKey("is_experimental").Value; set => manager.GetConfigByKey("is_experimental").Value = value; }
+
     public string[] Keywords { get => manager.GetConfigByKey("keywords").Value; set => manager.GetConfigByKey("keywords").Value = value; }
+
     public string Name { get => manager.GetConfigByKey("name").Value; set => manager.GetConfigByKey("name").Value = value; }
+
     public User Owner { get => AccountManagement.Instance.GetUser(manager.GetConfigByKey("owner").Value); set => manager.GetConfigByKey("owner").Value = value.Username; }
+
     public float Price { get => manager.GetConfigByKey("price").Value; set => manager.GetConfigByKey("price").Value = value; }
+
     public string Repository_URL { get => manager.GetConfigByKey("repository_url").Value; set => manager.GetConfigByKey("repository_url").Value = value; }
+
+    public Review[] Reviews { get => manager.GetConfigByKey("reviews").Value; set => manager.GetConfigByKey("reviews").Value = value; }
+
     public bool ShowGithub { get => manager.GetConfigByKey("show_github").Value; set => manager.GetConfigByKey("show_github").Value = value; }
+
     public string[] Tags { get => manager.GetConfigByKey("tags").Value; set => manager.GetConfigByKey("tags").Value = value; }
+
     public Version[] Versions { get => manager.GetConfigByKey("versions").Value; set => manager.GetConfigByKey("versions").Value = value; }
+
     public string YoutubeVideo { get => manager.GetConfigByKey("youtube_video").Value; set => manager.GetConfigByKey("youtube_video").Value = value; }
 
     #endregion Properties
@@ -142,6 +96,15 @@ public class Product
         list.AddRange(Versions);
         list.Add(new(Versions.Length + 1, name, type, supported_os));
         Versions = list.ToArray();
+    }
+
+    public Review LeaveReview(string shortDescription, string longDescription, User writer, byte star)
+    {
+        List<Review> list = Reviews.ToList();
+        Review review = new(shortDescription, longDescription, writer, DateTime.Now, this, star);
+        list.Add(review);
+        Reviews = list.ToArray();
+        return review;
     }
 
     public void RemoveKeyword(string keyword)
@@ -178,6 +141,7 @@ public class Product
         manager.Add("is_experimental", true);
         manager.Add("price", 0f);
         manager.Add("versions", Array.Empty<string>());
+        manager.Add("reviews", Array.Empty<object>());
     }
 
     #endregion Private Methods
