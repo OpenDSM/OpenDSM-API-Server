f79e2115d934c56141e4600d67de454c2776193d
Drew Chase <dcmanproductions@gmail.com>
2022-06-25 17:53:29 -0400

    Added Banner URL and YoutubeVideo Key

--- Library/Objects/Product.cs
+++ Library/Objects/Product.cs
@@ -100,14 +100,12 @@ public class Product
 
     #region Properties
 
+    public string Banner { get => manager.GetConfigByKey("banner_url").Value; set => manager.GetConfigByKey("banner_url").Value = value; }
     public string Description { get => manager.GetConfigByKey("description").Value; set => manager.GetConfigByKey("description").Value = value; }
 
-    public string Icon { get => manager.GetConfigByKey("icon_url").Value; set => manager.GetConfigByKey("description").Value = value; }
-
+    public string Icon { get => manager.GetConfigByKey("icon_url").Value; set => manager.GetConfigByKey("icon_url").Value = value; }
     public long ID { get => manager.GetConfigByKey("id").Value; set => manager.GetConfigByKey("id").Value = value; }
-
     public bool IsExperimental { get => manager.GetConfigByKey("is_experimental").Value; set => manager.GetConfigByKey("is_experimental").Value = value; }
-
     public string[] Keywords { get => manager.GetConfigByKey("keywords").Value; set => manager.GetConfigByKey("keywords").Value = value; }
     public string Name { get => manager.GetConfigByKey("name").Value; set => manager.GetConfigByKey("name").Value = value; }
     public User Owner { get => AccountManagement.Instance.GetUser(manager.GetConfigByKey("owner").Value); set => manager.GetConfigByKey("owner").Value = value.Username; }
@@ -116,6 +114,7 @@ public class Product
     public bool ShowGithub { get => manager.GetConfigByKey("show_github").Value; set => manager.GetConfigByKey("show_github").Value = value; }
     public string[] Tags { get => manager.GetConfigByKey("tags").Value; set => manager.GetConfigByKey("tags").Value = value; }
     public Version[] Versions { get => manager.GetConfigByKey("versions").Value; set => manager.GetConfigByKey("versions").Value = value; }
+    public string YoutubeVideo { get => manager.GetConfigByKey("youtube_video").Value; set => manager.GetConfigByKey("youtube_video").Value = value; }
 
     #endregion Properties
 
