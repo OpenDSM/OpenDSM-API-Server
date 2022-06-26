c62a286a206e166bc261e2f2f53c7e7c2b705c2f
Drew Chase <dcmanproductions@gmail.com>
2022-06-25 21:02:51 -0400

    Added search api

---
+++ API/Controllers/API/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenDSM.Lib.Objects;
+
+namespace API.Controllers.API;
+
+[ApiController]
+[Route("/api/search")]
+public class SearchController : ControllerBase
+{
+    #region Public Methods
+
+    [HttpGet()]
+    public IActionResult Index(string query, [FromQuery] string[] tags)
+    {
+        return new JsonResult(new
+        {
+            result = Products.Instance.Search(query, tags)
+        });
+    }
+
+    #endregion Public Methods
+}
