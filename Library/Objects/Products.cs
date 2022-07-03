82510001caf9876c8c6201e906a654dc60200751
dcmanproductions <dcmanproductions@gmail.com>
2022-07-02 23:53:13 -0400

    added create project function and changed id type from 64 bit longs to 32 bit unsigned interger

--- Library/Objects/Products.cs
+++ Library/Objects/Products.cs
@@ -7,7 +7,7 @@ public class Products
     #region Fields
 
     public static Products Instance = Instance ??= new();
-    private Dictionary<long, Product> _products;
+    private Dictionary<uint, Product> _products;
 
     #endregion Fields
 
@@ -22,31 +22,63 @@ public class Products
 
     #region Public Methods
 
-    public void Create()
+    public Product Create(User owner, string name, string description, string repo_url, bool isExperimental, bool showGithub, string[] keywords, string[] tags)
     {
+        uint id = (uint)(_products.Count() + 1);
+        Product product = new(id, owner, name, description, repo_url, isExperimental, showGithub, keywords, tags);
+        _products.Add(id, product);
+        return product;
+    }
+
+    public Product GetFeaturedProduct(User? user, bool force = false)
+    {
+        Product featured = _products[0];
+        if (user == null)
+        {
+            // No Algorithm
+        }
+        else
+        {
+            // Attempt Algorithm
+            if (force)
+            {
+            }
+        }
+        return featured;
     }
 
     public Product[] GetFromUser(User user)
     {
-        Product[] products = new Product[user.OwnedProducts.Length];
-        bool hadNull = false;
-        for (int i = 0; i < products.Length; i++)
+        try
         {
-            long productID = user.OwnedProducts[i];
-            if (_products.ContainsKey(productID))
+            if (user.OwnedProducts == null || !user.OwnedProducts.Any())
             {
-                products[i] = _products[user.OwnedProducts[i]];
+                return Array.Empty<Product>();
             }
-            else
+            Product[] products = new Product[user.OwnedProducts.Length];
+            bool hadNull = false;
+            for (uint i = 0; i < products.Length; i++)
             {
-                products[i] = null;
-                hadNull = true;
+                uint productID = user.OwnedProducts[i];
+                if (_products.ContainsKey(productID))
+                {
+                    products[i] = _products[user.OwnedProducts[i]];
+                }
+                else
+                {
+                    products[i] = null;
+                    hadNull = true;
+                }
             }
+            return hadNull ? products.Where(i => i != null).ToArray() : products;
         }
-        return hadNull ? products.Where(i => i != null).ToArray() : products;
+        catch
+        {
+            return Array.Empty<Product>();
+        }
     }
 
-    public Product GetProduct(long id)
+    public Product GetProduct(uint id)
     {
         return _products[id];
     }
@@ -57,7 +89,7 @@ public class Products
         Parallel.ForEach(files, file =>
         {
             FileInfo fileInfo = new(file);
-            if (int.TryParse(fileInfo.Name.Replace(fileInfo.Extension, ""), out int id))
+            if (uint.TryParse(fileInfo.Name.Replace(fileInfo.Extension, ""), out uint id))
             {
                 _products.Add(id, new(id));
             }
@@ -68,7 +100,7 @@ public class Products
     {
         Dictionary<int, Product> products = new();
         string[] keywords = query.Split(' ');
-        foreach ((long _, Product product) in _products)
+        foreach ((uint _, Product product) in _products)
         {
             int matches = 0;
             bool hasMatchingTags = true;
@@ -95,7 +127,7 @@ public class Products
                 }
             }
         }
-        return ((Dictionary<int, Product>)products.OrderBy(i => i.Key)).Values.ToArray();
+        return ((Dictionary<uint, Product>)products.OrderBy(i => i.Key)).Values.ToArray();
     }
 
     #endregion Public Methods
