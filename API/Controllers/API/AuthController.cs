c6a39c34c57736532d207ae5ecab8926921c9d11
dcmanproductions <dcmanproductions@gmail.com>
2022-07-02 23:46:08 -0400

    added save profile controller

--- API/Controllers/API/AuthController.cs
+++ API/Controllers/API/AuthController.cs
@@ -26,23 +26,69 @@ public class AuthController : ControllerBase
         return new JsonResult(new
         {
             Success = false,
-            reason,
+            message = reason,
         });
     }
 
     [HttpPost("register")]
     public IActionResult Register([FromForm] string username, [FromForm] string email, [FromForm] string password)
     {
-        User user = AccountManagement.Instance.CreateUser(email, username, password);
-
-        return new JsonResult(new
+        try
         {
-            user.ID,
-            user.Username,
-            user.Email,
-            Token = user.Password,
-            Success = true,
-        });
+            if (AccountManagement.Instance.CreateUser(email, username, password, out User user, out string reason))
+            {
+                return new JsonResult(new
+                {
+                    user.ID,
+                    user.Username,
+                    user.Email,
+                    Token = user.Password,
+                    Success = true,
+                });
+            }
+            return new JsonResult(new
+            {
+                success = false,
+                message = reason
+            });
+        }
+        catch (Exception ex)
+        {
+            log.Error(ex.Message, ex.StackTrace);
+            return BadRequest(new
+            {
+                success = false,
+                message = "Unknown Server Issue"
+            });
+        }
+    }
+
+    [HttpPost("save-profile/{key}")]
+    public IActionResult SaveProfile(string key, [FromForm] string value)
+    {
+        string token = Request.Cookies["auth_token"] ?? "";
+        string username = Request.Cookies["auth_username"] ?? "";
+        if (!string.IsNullOrEmpty(token) && !string.IsNullOrEmpty(username))
+        {
+            if (AccountManagement.Instance.TryAttemptLogin(username, token, out User user))
+            {
+                switch (key.ToLower())
+                {
+                    case "image":
+                        break;
+
+                    case "show_github":
+                        user.ShowGithub = bool.Parse(value);
+                        break;
+
+                    default:
+                        break;
+                }
+                return Ok();
+            }
+        }
+
+        return BadRequest();
     }
 
     #endregion Public Methods
