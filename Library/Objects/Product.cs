cceb2f5e50c713c26ed502c1f4360680fa2864a0
dcmanproductions <dcmanproductions@gmail.com>
2022-07-02 23:51:35 -0400

    updated config manager pattern

--- Library/Objects/Product.cs
+++ Library/Objects/Product.cs
@@ -13,10 +13,9 @@ public class Product
 
     #region Public Constructors
 
-    public Product(long id, User owner, string name, string description, string repo_url, bool isExperimental, bool showGithub, string[] keywords, string[] tags, params Version[] versions)
+    public Product(uint id, User owner, string name, string description, string repo_url, bool isExperimental, bool showGithub, string[] keywords, string[] tags, params Version[] versions)
     {
         manager = new(Path.Combine(ProductsDirectory, $"{id}.json"));
-        Init();
 
         ID = id;
         Name = name;
@@ -30,45 +29,34 @@ public class Product
         Tags = tags;
     }
 
-    public Product(long id)
+    public Product(uint id)
     {
         manager = new(Path.Combine(ProductsDirectory, $"{id}.json"));
-        Init();
     }
 
     #endregion Public Constructors
 
     #region Properties
 
-    public string Banner { get => manager.GetConfigByKey("banner_url").Value; set => manager.GetConfigByKey("banner_url").Value = value; }
-
-    public string Description { get => manager.GetConfigByKey("description").Value; set => manager.GetConfigByKey("description").Value = value; }
-
-    public string Icon { get => manager.GetConfigByKey("icon_url").Value; set => manager.GetConfigByKey("icon_url").Value = value; }
-
-    public long ID { get => manager.GetConfigByKey("id").Value; set => manager.GetConfigByKey("id").Value = value; }
-
-    public bool IsExperimental { get => manager.GetConfigByKey("is_experimental").Value; set => manager.GetConfigByKey("is_experimental").Value = value; }
-
-    public string[] Keywords { get => manager.GetConfigByKey("keywords").Value; set => manager.GetConfigByKey("keywords").Value = value; }
-
-    public string Name { get => manager.GetConfigByKey("name").Value; set => manager.GetConfigByKey("name").Value = value; }
-
-    public User Owner { get => AccountManagement.Instance.GetUser(manager.GetConfigByKey("owner").Value); set => manager.GetConfigByKey("owner").Value = value.Username; }
-
-    public float Price { get => manager.GetConfigByKey("price").Value; set => manager.GetConfigByKey("price").Value = value; }
-
-    public string Repository_URL { get => manager.GetConfigByKey("repository_url").Value; set => manager.GetConfigByKey("repository_url").Value = value; }
-
-    public Review[] Reviews { get => manager.GetConfigByKey("reviews").Value; set => manager.GetConfigByKey("reviews").Value = value; }
-
-    public bool ShowGithub { get => manager.GetConfigByKey("show_github").Value; set => manager.GetConfigByKey("show_github").Value = value; }
-
-    public string[] Tags { get => manager.GetConfigByKey("tags").Value; set => manager.GetConfigByKey("tags").Value = value; }
-
-    public Version[] Versions { get => manager.GetConfigByKey("versions").Value; set => manager.GetConfigByKey("versions").Value = value; }
-
-    public string YoutubeVideo { get => manager.GetConfigByKey("youtube_video").Value; set => manager.GetConfigByKey("youtube_video").Value = value; }
+    public string Banner { get => manager.GetOrCreate("banner_url", "").Value; set => manager.GetOrCreate("banner_url", "").Value = value; }
+    public string Description { get => manager.GetOrCreate("description", "").Value; set => manager.GetOrCreate("description", "").Value = value; }
+    public string Icon { get => manager.GetOrCreate("icon_url", "").Value; set => manager.GetOrCreate("icon_url", "").Value = value; }
+    public uint ID { get => manager.GetOrCreate("id", 0).Value; set => manager.GetOrCreate("id", 0).Value = value; }
+    public bool IsExperimental { get => manager.GetOrCreate("is_experimental", true).Value; set => manager.GetOrCreate("is_experimental", true).Value = value; }
+    public string[] Keywords { get => manager.GetOrCreate("keywords", Array.Empty<string>()).Value; set => manager.GetOrCreate("keywords", Array.Empty<string>()).Value = value; }
+    public string Name { get => manager.GetOrCreate("name", "").Value; set => manager.GetOrCreate("name", "").Value = value; }
+    public User Owner { get => AccountManagement.Instance.GetUser(manager.GetOrCreate("owner", "").Value); set => manager.GetOrCreate("owner", "").Value = value.Username; }
+    public float Price { get => manager.GetOrCreate("price", 0f).Value; set => manager.GetOrCreate("price", 0f).Value = value; }
+    public string Repository_URL { get => manager.GetOrCreate("repository_url", "").Value; set => manager.GetOrCreate("repository_url", "").Value = value; }
+    public Review[] Reviews { get => manager.GetOrCreate("reviews", Array.Empty<object>()).Value; set => manager.GetOrCreate("reviews", Array.Empty<object>()).Value = value; }
+    public bool ShowGithub { get => manager.GetOrCreate("show_github", false).Value; set => manager.GetOrCreate("show_github", false).Value = value; }
+    public string[] Tags { get => manager.GetOrCreate("tags", Array.Empty<string>()).Value; set => manager.GetOrCreate("tags", Array.Empty<string>()).Value = value; }
+    public uint TotalDownloads { get => manager.GetOrCreate("total_downloads", 0).Value; set => manager.GetOrCreate("total_downloads", 0).Value = value; }
+    public uint TotalViews { get => manager.GetOrCreate("total_views", 0).Value; set => manager.GetOrCreate("total_views", 0).Value = value; }
+    public Version[] Versions { get => manager.GetOrCreate("versions", Array.Empty<object>()).Value; set => manager.GetOrCreate("versions", Array.Empty<object>()).Value = value; }
+    public uint WeeklyDownloads { get => manager.GetOrCreate("weekly_downloads", 0).Value; set => manager.GetOrCreate("weekly_downloads", 0).Value = value; }
+    public uint WeeklyViews { get => manager.GetOrCreate("weekly_views", 0).Value; set => manager.GetOrCreate("weekly_views", 0).Value = value; }
+    public string YoutubeVideo { get => manager.GetOrCreate("youtube_video", "").Value; set => manager.GetOrCreate("youtube_video", "").Value = value; }
 
     #endregion Properties
 
@@ -94,7 +82,7 @@ public class Product
     {
         List<Version> list = new();
         list.AddRange(Versions);
-        list.Add(new(Versions.Length + 1, name, type, supported_os));
+        list.Add(new((uint)(Versions.Length + 1), name, type, supported_os));
         Versions = list.ToArray();
     }
 
@@ -124,25 +112,4 @@ public class Product
     }
 
     #endregion Public Methods
-
-    #region Private Methods
-
-    private void Init()
-    {
-        manager.Add("id", -1);
-        manager.Add("name", "");
-        manager.Add("description", "");
-        manager.Add("owner", "");
-        manager.Add("repository_url", "");
-        manager.Add("icon_url", "");
-        manager.Add("tags", Array.Empty<string>());
-        manager.Add("keywords", Array.Empty<string>());
-        manager.Add("show_github", false);
-        manager.Add("is_experimental", true);
-        manager.Add("price", 0f);
-        manager.Add("versions", Array.Empty<string>());
-        manager.Add("reviews", Array.Empty<object>());
-    }
-
-    #endregion Private Methods
 }
