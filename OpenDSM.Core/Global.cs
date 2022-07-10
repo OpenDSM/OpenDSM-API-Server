bb8b295b5fbefb8e0c921b4c4c010f75580fb44b
dcmanproductions <dcmanproductions@gmail.com>
2022-07-09 21:39:14 -0400

    Restarting

---
+++ OpenDSM.Core/Global.cs
@@ -0,0 +1,24 @@
+﻿global using static OpenDSM.Core.Global;
+
+using ChaseLabs.CLLogger;
+using ChaseLabs.CLLogger.Interfaces;
+namespace OpenDSM.Core;
+
+public static class Global
+{
+    public static ILog log = LogManager.Init().SetDumpMethod(1000).SetLogDirectory(LogsDirectory);
+    public static ILog log_user = LogManager.Init().SetDumpMethod(1000).SetLogDirectory(UserLogsDirectory);
+    public static ILog log_access = LogManager.Init().SetDumpMethod(1000).SetLogDirectory(AccessLogsDirectory);
+    public static ILog log_admin = LogManager.Init().SetDumpMethod(1000).SetLogDirectory(AccessLogsDirectory);
+    public static ILog log_sales = LogManager.Init().SetDumpMethod(1000).SetLogDirectory(SalesLogsDirectory);
+    public static string ApplicationName => "OpenDSM";
+    public static string CompanyName => "LFInteractive";
+    public static string Copywrite => $"All Rights Reserved - {CompanyName} LLC. ©️ 2021-{DateTime.Now.Year}";
+    public static string RootDirectory => Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), CompanyName, ApplicationName)).FullName;
+    public static string LogsDirectory => Directory.CreateDirectory(Path.Combine(RootDirectory, "Logs")).FullName;
+    public static string UserLogsDirectory => Directory.CreateDirectory(Path.Combine(LogsDirectory, "Users")).FullName;
+    public static string AccessLogsDirectory => Directory.CreateDirectory(Path.Combine(LogsDirectory, "Access")).FullName;
+    public static string AdminLogsDirectory => Directory.CreateDirectory(Path.Combine(LogsDirectory, "Admin")).FullName;
+    public static string SalesLogsDirectory => Directory.CreateDirectory(Path.Combine(LogsDirectory, "Sales")).FullName;
+
+}
---
+++ OpenDSM.Server/Program.cs
@@ -0,0 +1,89 @@
+global using static OpenDSM.Core.Global;
+using ChaseLabs.CLLogger;
+using ChaseLabs.CLLogger.Interfaces;
+using CLPortmapper;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace OpenDSM.Server;
+
+public class Program
+{
+    #region Fields
+
+    private static int port = 8256;
+
+    #endregion Fields
+
+    #region Private Methods
+
+    private static void Main(string[] args)
+    {
+        log.Info("Welcome to Kestrel Template");
+        log.Debug("All Rights Reserved - LFInteractive LLC. (c) 2020-2022");
+        if (args.Length == 0)
+        {
+            PortHandler.AddToFirewall();
+            log.Warn($"Automatic Port Forwarding is Enabled");
+            log.Debug($"Opening Port {port}");
+            PortHandler.OpenPort(port);
+            Host.CreateDefaultBuilder().ConfigureWebHostDefaults(builder =>
+            {
+                log.Info($"Starting server on port {port}");
+                builder.UseIISIntegration();
+                builder.UseContentRoot(Directory.GetCurrentDirectory());
+                builder.UseKestrel(options =>
+                {
+                    options.ListenAnyIP(port);
+                });
+                builder.UseStartup<Startup>();
+                log.Info("Server is now running!");
+                new Process()
+                {
+                    StartInfo = new()
+                    {
+                        FileName = $"http://127.0.0.1:{port}/api/template",
+                        UseShellExecute = true,
+                    }
+                }.Start();
+            }).Build().Run();
+
+            log.Info("Shutting Down");
+            PortHandler.ClosePort(port);
+        }
+        else
+        {
+            if (args[0] == "--firewall")
+            {
+                PortHandler.AddToFirewall();
+            }
+        }
+    }
+
+    #endregion Private Methods
+}
+
+internal class Startup
+{
+    #region Public Methods
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment evn)
+    {
+        app.UseForwardedHeaders();
+        app.UseMvc();
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseDefaultFiles();
+    }
+
+    public void ConfigureServices(IServiceCollection service)
+    {
+        service.AddMvc(action =>
+        {
+            action.EnableEndpointRouting = false;
+        });
+    }
+
+    #endregion Public Methods
+}
