e288ac9d5553c21e99974157f41a19626e3a5e1b
dcmanproductions <dcmanproductions@gmail.com>
2022-07-10 23:18:00 -0400

    Added Product Model

---
+++ OpenDSM.Core/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDSM.Core.Models;
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public int UserID { get; set; }
+    public string Name { get; set; }
+    public string About { get; set; }
+    public int TotalDownloads { get; set; }
+    public string VideoURL { get; set; }
+}
