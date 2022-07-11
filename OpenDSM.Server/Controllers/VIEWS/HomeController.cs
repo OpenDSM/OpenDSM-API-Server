50a97ab47bfde117e20edb8470920bad73ec6589
dcmanproductions <dcmanproductions@gmail.com>
2022-07-10 21:00:20 -0400

    Added Home Controller

---
+++ OpenDSM.Server/Controllers/VIEWS/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenDSM.Core.Models;
+
+namespace OpenDSM.Server.Controllers.VIEWS;
+
+[Route("/")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "The Open Digital Software Marketplace";
+        if(AuthController.IsLoggedIn(Request.Cookies, out UserModel? user))
+        {
+            ViewData["User"] = user;
+        }
+        return View();
+    }
+}
