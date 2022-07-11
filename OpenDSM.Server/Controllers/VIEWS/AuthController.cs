14af4ed719916aba65fa417b3a023af7c170ee7a
dcmanproductions <dcmanproductions@gmail.com>
2022-07-10 21:03:07 -0400

    Added authentication controller

---
+++ OpenDSM.Server/Controllers/VIEWS/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenDSM.Core.Models;
+
+namespace OpenDSM.Server.Controllers.VIEWS;
+
+[Route("/auth")]
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public static bool IsLoggedIn(IRequestCookieCollection cookies, out UserModel? user)
+    {
+        user = null;
+        string username = cookies["auth_username"] ?? "";
+        string token = cookies["auth_token"] ?? "";
+        if (!(string.IsNullOrEmpty(username) && string.IsNullOrWhiteSpace(token)))
+        {
+            return UserModel.TryGetUser(username, token, out user);
+        }
+
+        return false;
+    }
+}
