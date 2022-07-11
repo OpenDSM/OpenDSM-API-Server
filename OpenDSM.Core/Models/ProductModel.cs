1cab99dbb47b7d16d9fa2728220f44830ded0bb8
dcmanproductions <dcmanproductions@gmail.com>
2022-07-11 12:48:06 -0400

    Added constructor

--- OpenDSM.Core/Models/ProductModel.cs
+++ OpenDSM.Core/Models/ProductModel.cs
@@ -14,5 +14,22 @@ public class ProductModel
     public string About { get; private set; }
     public int TotalDownloads { get; private set; }
     public string VideoURL { get; private set; }
+    public uint Price { get; private set; }
     public UserModel User { get; private set; }
+
+    protected ProductModel(int id, int userID, string name, string about, int totalDownloads, string videoURL, UserModel user, uint price)
+    {
+        Id = id;
+        UserID = userID;
+        Name = name;
+        About = about;
+        TotalDownloads = totalDownloads;
+        VideoURL = videoURL;
+        User = user;
+        Price = price;
+    }
+    public static ProductModel? GetByID(int id)
+    {
+        return null;
+    }
 }
