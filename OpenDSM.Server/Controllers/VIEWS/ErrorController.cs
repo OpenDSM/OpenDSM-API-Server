52124840bd56f84b52444d686179d9e6dfd0fe13
dcmanproductions <dcmanproductions@gmail.com>
2022-07-10 20:58:58 -0400

    Added custom error page

---
+++ OpenDSM.Server/Controllers/VIEWS/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenDSM.Server.Controllers.VIEWS;
+
+[Route("Error")]
+public class ErrorController : Controller
+{
+    [Route("{code}")]
+    public IActionResult Index(int code)
+    {
+        ViewData["Title"] = $"{code}";
+        return View(code);
+    }
+}
