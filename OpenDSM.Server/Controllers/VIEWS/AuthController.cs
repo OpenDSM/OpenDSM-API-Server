a76771aaec1f00eae542665714c9c24b3c7c8532
dcmanproductions <dcmanproductions@gmail.com>
2022-07-12 13:27:49 -0400

    Added Login and Signup Pages

--- OpenDSM.Server/Controllers/VIEWS/AuthController.cs
+++ OpenDSM.Server/Controllers/VIEWS/AuthController.cs
@@ -6,19 +6,60 @@ namespace OpenDSM.Server.Controllers.VIEWS;
 [Route("/auth")]
 public class AuthController : Controller
 {
-    public IActionResult Index()
+    [Route("login")]
+    public IActionResult Login()
     {
+        ViewData["Title"] = "Login";
+        if (IsLoggedIn(Request.Cookies, out UserModel? user))
+        {
+            return RedirectToAction("Index", "Home");
+        }
         return View();
+    }
+    [Route("signup")]
+    public IActionResult Signup()
+    {
+        ViewData["Title"] = "Signup";
+        if (IsLoggedIn(Request.Cookies, out UserModel? _))
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        return View();
+    }
+
+    [Route("profile")]
+    public IActionResult GetProfile()
+    {
+        if (IsLoggedIn(Request.Cookies, out UserModel? user))
+        {
+            ViewData["Title"] = $"{user?.Username}'s Profile";
+            ViewData["User"] = user;
+            return View(user);
+        }
+        return RedirectToAction("Index", "Home");
+    }
+
+    [Route("profile/{id}")]
+    public IActionResult GetUsersProfile(int id)
+    {
+        if (IsLoggedIn(Request.Cookies, out UserModel? loggedin))
+        {
+            ViewData["User"] = loggedin;
+        }
+        UserModel? user = UserModel.GetByID(id);
+        if (user == null)
+            return RedirectToAction("Index", "Error", new { code = 404 });
+        return View(user);
     }
 
     public static bool IsLoggedIn(IRequestCookieCollection cookies, out UserModel? user)
     {
         user = null;
-        string username = cookies["auth_username"] ?? "";
+        string email = cookies["auth_email"] ?? "";
         string token = cookies["auth_token"] ?? "";
-        if (!(string.IsNullOrEmpty(username) && string.IsNullOrWhiteSpace(token)))
+        if (!string.IsNullOrEmpty(email) && !string.IsNullOrWhiteSpace(token))
         {
-            return UserModel.TryGetUser(username, token, out user);
+            return UserModel.TryGetUserWithToken(email, token, out user);
         }
 
         return false;
