7745147364e04f49ec60ff2a7935b72c76013125
dcmanproductions <dcmanproductions@gmail.com>
2022-07-17 21:44:20 -0400

    Added Check for Github Username and Token, and added about section

--- OpenDSM.Core/Models/UserModel.cs
+++ OpenDSM.Core/Models/UserModel.cs
@@ -1,7 +1,9 @@
-﻿using OpenDSM.SQL;
+﻿using Newtonsoft.Json.Linq;
+using OpenDSM.SQL;
 
 namespace OpenDSM.Core.Models;
 
+public record GitRepository(int ID, string Name);
 public class UserModel
 {
     #region Protected Constructors
@@ -17,16 +19,24 @@ public class UserModel
         CreatedProducts = Products.GetProductsByOwner(id);
         ProfileImage = Path.Combine(GetUsersProfileDirectory(Id), "profile.jpg");
         ProfileBannerImage = Path.Combine(GetUsersProfileDirectory(Id), "banner.jpg");
+        string aboutPath = Path.Combine(GetUsersProfileDirectory(Id), "about.md");
+        aboutPath = File.Exists(aboutPath) ? aboutPath : "./wwwroot/assets/md/default_about.md";
+        using (FileStream fs = new(aboutPath, FileMode.Open, FileAccess.Read))
+        {
+            using StreamReader reader = new(fs);
+            About = reader.ReadToEnd();
+        }
     }
 
     #endregion Protected Constructors
 
     #region Public Properties
 
-    public string About { get; set; }
+    public string About { get; private set; }
     public int[] CreatedProducts { get; private set; }
     public string Email { get; private set; }
-    public string GitToken { get; private set; }
+    public string GitToken { get; set; }
+    public string GitUsername { get; set; }
     public int Id { get; private set; }
     public int[] OwnedProducts { get; private set; }
     public string ProfileBannerImage { get; set; }
@@ -34,6 +44,59 @@ public class UserModel
     public string Token { get; private set; }
     public AccountType Type { get; private set; }
     public string Username { get; private set; }
+
+    public bool IsDeveloperAccount
+    {
+        get
+        {
+            if (!string.IsNullOrWhiteSpace(GitToken) && !string.IsNullOrWhiteSpace(GitUsername))
+            {
+                HttpClient client = new();
+                client.DefaultRequestHeaders.Add("Authorization", $"token {GitToken}");
+                client.DefaultRequestHeaders.Add("User-Agent", $"OpenDSM");
+                HttpResponseMessage response = client.GetAsync($"https://api.github.com/users/{GitUsername}/repos").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.Content != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public GitRepository[] Repositories
+    {
+        get
+        {
+            List<GitRepository> repos = new();
+            if (!string.IsNullOrWhiteSpace(GitToken) && !string.IsNullOrWhiteSpace(GitUsername))
+            {
+                HttpClient client = new();
+                client.DefaultRequestHeaders.Add("Authorization", $"token {GitToken}");
+                client.DefaultRequestHeaders.Add("User-Agent", $"OpenDSM");
+                HttpResponseMessage response = client.GetAsync($"https://api.github.com/users/{GitUsername}/repos").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.Content != null)
+                    {
+                        JArray jArray = JArray.Parse(response.Content.ReadAsStringAsync().Result);
+                        foreach (JToken token in jArray)
+                        {
+                            JObject jObject = JObject.FromObject(token);
+                            if (int.TryParse(jObject["id"].ToString(), out int id))
+                            {
+                                repos.Add(new(id, jObject["name"].ToString()));
+                            }
+                        }
+                    }
+                }
+            }
+            return repos.ToArray();
+        }
+    }
 
     #endregion Public Properties
 
@@ -50,9 +113,13 @@ public class UserModel
 
     public static UserModel? GetUser(string username, string password, out FailedReason reason)
     {
-        if (Authoriztaion.Login(username, password, out reason, out AccountType type, out int id, out string email, out string uname, out string token, out int[] products))
+        if (Authoriztaion.Login(username, password, out reason, out AccountType type, out int id, out string email, out string uname, out string token, out int[] products, out string r_git_username, out string r_git_token))
         {
-            return new(id, uname, email, token, type, products);
+            return new(id, uname, email, token, type, products)
+            {
+                GitToken = r_git_token,
+                GitUsername = r_git_username
+            };
         }
         return null;
     }
@@ -71,9 +138,13 @@ public class UserModel
     public static bool TryGetUserWithToken(string email, string password, out UserModel? user)
     {
         user = null;
-        if (Authoriztaion.LoginWithToken(email, password, out var reason, out AccountType type, out int id, out string r_email, out string uname, out string token, out int[] products))
+        if (Authoriztaion.LoginWithToken(email, password, out var reason, out AccountType type, out int id, out string r_email, out string uname, out string token, out int[] products, out string r_git_username, out string r_git_token))
         {
-            user = new(id, uname, email, token, type, products);
+            user = new(id, uname, email, token, type, products)
+            {
+                GitToken = r_git_token,
+                GitUsername = r_git_username
+            }; ;
             return true;
         }
         return false;
@@ -81,7 +152,20 @@ public class UserModel
 
     public void UpdateSetting(string name, dynamic value)
     {
-        Authoriztaion.UpdateProperty(Token, name, value);
+        Authoriztaion.UpdateProperty(Id, Token, name, value);
+    }
+    public void UpdateAbout(string markdown)
+    {
+        if (About != markdown)
+        {
+            string aboutPath = Path.Combine(GetUsersProfileDirectory(Id), "about.md");
+            using (FileStream fs = new(aboutPath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using StreamWriter writer = new(fs);
+                writer.Write(markdown);
+            }
+            About = markdown;
+        }
     }
 
     public async Task UploadImage(string base64, bool isProfile)
