325043e41f16203c76768962cbfbc2243e1238d6
dcmanproductions <dcmanproductions@gmail.com>
2022-07-19 10:56:22 -0400

    Moved upload gallery functionality to a function

--- OpenDSM.Server/wwwroot/assets/js/create-product.js
+++ OpenDSM.Server/wwwroot/assets/js/create-product.js
@@ -9,16 +9,42 @@ $("#upload-icon.file-upload").on('click', e => {
     input.accept = "image/*";
     $(input).on('change', l => {
         let file = l.currentTarget.files[0];
-        let popup = new ImagePopup(file, 1 / 1, true, async () => {
-            let data = new FormData();
-            data.append("base64", $(this.image).data('cropper').getCroppedCanvas().toDataURL().split('base64,')[1]);
-            data.append("email", this.email);
-            data.append("token", this.token);
-            await fetch('/api/auth/image/profile', { method: "POST", body: data })
-
-            window.location.reload();
-        });
+        let popup = new ImagePopup(e.currentTarget, file, 1 / 1, false, async base => { });
         popup.open();
     })
     input.click();
 })
+$("#upload-banner.file-upload").on('click', e => {
+    let input = document.createElement('input');
+    input.type = "file"
+    input.accept = "image/*";
+    $(input).on('change', l => {
+        let file = l.currentTarget.files[0];
+        let popup = new ImagePopup(e.currentTarget, file, 16 / 9, false, async base => { });
+        popup.open();
+    })
+    input.click();
+})
+
+function UploadGalleryImage(target) {
+
+    let input = document.createElement('input');
+    input.type = "file"
+    input.accept = "image/*";
+    $(input).on('change', l => {
+        let file = l.currentTarget.files[0];
+        let popup = new ImagePopup(target, file, 16 / 9, false, async base => {
+            target.parentElement.parentElement.innerHTML += `
+            <div class="list vertical">
+                    <h4 class="file-upload-title">${file.name}</h4>
+                    <div class="file-upload tmp-gallery-image" onclick="this.parentElement.remove()" style="background-image: url('data:image/png;base64,${base}')">
+                        <div class="content">
+                            <i class="fas fa-trash"></i>
+                        </div>
+                    </div>
+                </div>`;
+        });
+        popup.open();
+    })
+    input.click();
+}
