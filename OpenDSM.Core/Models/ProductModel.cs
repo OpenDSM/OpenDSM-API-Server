57267d05d71bf992df3aace1e2bd3479562519ce
dcmanproductions <dcmanproductions@gmail.com>
2022-07-19 10:50:30 -0400

    Added getter for youtube video direct url

--- OpenDSM.Core/Models/ProductModel.cs
+++ OpenDSM.Core/Models/ProductModel.cs
@@ -1,4 +1,8 @@
 ﻿using OpenDSM.SQL;
+using YoutubeExplode;
+using System.Linq;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
 
 namespace OpenDSM.Core.Models;
 
@@ -6,16 +10,39 @@ public class ProductModel
 {
     #region Protected Constructors
 
-    protected ProductModel(int id, int userID, string name, string about, int totalDownloads, string videoURL, UserModel user, uint price)
+    protected ProductModel(int id, int userID, string name, string about, int totalDownloads, string videoKey, UserModel user, uint price)
     {
         Id = id;
         UserID = userID;
         Name = name;
         About = about;
         TotalDownloads = totalDownloads;
-        VideoURL = videoURL;
         User = user;
         Price = price;
+
+        try
+        {
+            string youtubeURL = $"https://youtube.com/watch?v={videoKey}";
+            YoutubeClient client = new();
+            VideoClient videoClient = client.Videos;
+            StreamClient videoStreams = videoClient.Streams;
+            StreamManifest? manifest = videoStreams.GetManifestAsync(youtubeURL).Result;
+            IEnumerable<MuxedStreamInfo> muxedStreams = manifest.GetMuxedStreams();
+
+            string url = "";
+            int area = -1;
+            foreach (MuxedStreamInfo videoStream in from videoStream in muxedStreams where videoStream.VideoResolution.Area > area select videoStream)
+            {
+                area = videoStream.VideoResolution.Area;
+                url = videoStream.Url;
+            }
+
+            VideoURL = url;
+        }
+        catch
+        {
+            VideoURL = "";
+        }
     }
 
     #endregion Protected Constructors
