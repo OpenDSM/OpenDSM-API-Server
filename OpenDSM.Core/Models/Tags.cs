5f2a4e60e6bea772bb75b5d4efe0e32f9b7b719b
dcmanproductions <dcmanproductions@gmail.com>
2022-07-19 14:29:58 -0400

    Added Tags

---
+++ OpenDSM.Core/Models/Tags.cs
@@ -0,0 +1,36 @@
+﻿namespace OpenDSM.Core.Models;
+
+public record Tag(int id, string name);
+
+public class Tags
+{
+    #region Private Fields
+
+    private static Tags Instance = Instance ??= new();
+    private readonly Tag[] tags;
+
+    #endregion Private Fields
+
+    #region Private Constructors
+
+    private Tags()
+    {
+        Dictionary<int, string> TagDictionary = SQL.Tags.GetTags();
+        tags = new Tag[TagDictionary.Count];
+        for (int i = 0; i < tags.Length; i++)
+        {
+            tags[i] = new Tag(i, TagDictionary[i]);
+        }
+    }
+
+    #endregion Private Constructors
+
+    #region Public Methods
+
+    public static Tag[] GetTags()
+    {
+        return Instance.tags;
+    }
+
+    #endregion Public Methods
+}
