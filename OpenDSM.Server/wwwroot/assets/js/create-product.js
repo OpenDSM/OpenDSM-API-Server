d28634b916600ab5dcf2bbe5e78c5ef899198edc
dcmanproductions <dcmanproductions@gmail.com>
2022-07-22 11:37:55 -0400

    Added required fields

--- OpenDSM.Server/wwwroot/assets/js/create-product.js
+++ OpenDSM.Server/wwwroot/assets/js/create-product.js
@@ -26,6 +26,50 @@ $("#upload-banner.file-upload").on('click', e => {
     input.click();
 })
 
+$("#yt-key-box").on('focusin', async e => {
+    try {
+        let clipboard = await navigator.clipboard.readText();
+        if (clipboard.includes("youtube.com/watch?v=")) {
+            let key = clipboard.split("watch?v=")[1].split("&")[0].replace("watch?v=", "").replace("&", "")
+            e.currentTarget.value = key;
+        }
+    } catch (ex) {
+        console.error(ex);
+    }
+})
+
+$("#test-video-btn").on('click', () => {
+    let popup = new VideoPopup($("#yt-key-box")[0].value);
+    popup.open();
+})
+$("#accept-tos-toggle").on('click', e => {
+    let value = $(e.currentTarget).attr('value') == "true";
+    $("#submit-btn")[0].disabled = value;
+})
+$("#submit-btn").on('click', async () => {
+    let required = Array.from($("[required]"));
+    let requirements = "";
+    let missing = 0;
+    for (let i = 0; i < required.length; i++) {
+        let value = required[i].value == null ? $(required[i]).attr('value') : required[i].value;
+        if (value == "" || value == null) {
+            missing++;
+            requirements += `<li>${$(`label[for="${required[i].id}"]`)[0].innerHTML}</li>`
+        }
+    }
+    if (missing > 0) {
+        await new ErrorPopup("Missing Requirement(s)", `<h4><ul>${requirements}</ul></h4>`).open();
+    } else {
+        let repoName = $("#github-repository-search-box")[0].value;
+        let projectName = $("#project-name-box")[0].value;
+        let tags = $("#tags-search-box")[0].value.split("; ");
+        let keywords = $("#")
+
+        let data = new FormData();
+
+    }
+});
+
 function UploadGalleryImage(target) {
 
     let input = document.createElement('input');
