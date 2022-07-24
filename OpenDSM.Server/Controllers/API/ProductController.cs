4075f1704b48202c733876a3a3543743370233a0
dcmanproductions <dcmanproductions@gmail.com>
2022-07-24 18:18:55 -0400

    added try catch to create product controller

--- OpenDSM.Server/Controllers/API/ProductController.cs
+++ OpenDSM.Server/Controllers/API/ProductController.cs
@@ -1,6 +1,6 @@
 ﻿// LFInteractive LLC. (c) 2021-2022 - All Rights Reserved
 using Microsoft.AspNetCore.Mvc;
-using OpenDSM.Core;
+using OpenDSM.Core.Handlers;
 using OpenDSM.Core.Models;
 
 namespace OpenDSM.Server.Controllers.API;
@@ -13,24 +13,31 @@ public class ProductController : ControllerBase
     [HttpPost("create")]
     public IActionResult CreateProduct([FromForm] string name, [FromForm] string gitRepoName, [FromForm] int user_id, [FromForm] string? yt_key, [FromForm] bool subscription, [FromForm] bool use_git_readme, [FromForm] float price, [FromForm] string keywords, [FromForm] string tags, [FromForm] string icon, [FromForm] string banner, [FromForm] string[]? gallery)
     {
-        List<int> ts = new();
-        foreach (var item in tags.Split(";", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+        try
         {
-            foreach (var tag in Tags.GetTags())
+            List<int> ts = new();
+            foreach (var item in tags.Split(";", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
             {
-                if (tag.name == item) ts.Add(tag.id);
-            }
+                foreach (var tag in Tags.GetTags())
+                {
+                    if (tag.name == item) ts.Add(tag.id);
+                }
 
-        }
-        if (ProductModel.TryCreateProduct(gitRepoName, UserModel.GetByID(user_id), name, yt_key ?? "", subscription, use_git_readme, (int)(price * 100), keywords.Split(";", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries), ts.ToArray(), out ProductModel model))
-        {
-            model.IconUrl = icon;
-            model.BannerImage = banner;
-            if (gallery != null)
-            {
-                model.GalleryImages = gallery;
             }
-            return new JsonResult(model);
+            if (ProductModel.TryCreateProduct(gitRepoName, UserModel.GetByID(user_id), name, yt_key ?? "", subscription, use_git_readme, (int)(price * 100), keywords.Split(";", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries), ts.ToArray(), out ProductModel model))
+            {
+                model.IconUrl = icon;
+                model.BannerImage = banner;
+                if (gallery != null && gallery.Any())
+                {
+                    model.GalleryImages = gallery;
+                }
+                return new JsonResult(model);
+            }
+        }
+        catch (Exception e)
+        {
+            log.Error(e.Message, e);
         }
         return BadRequest();
     }
