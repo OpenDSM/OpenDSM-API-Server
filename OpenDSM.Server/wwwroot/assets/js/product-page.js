e33e6248b07db829e69733cd555504cb9122a795
dcmanproductions <dcmanproductions@gmail.com>
2022-07-30 00:25:23 -0400

    Added product-page js file

---
+++ OpenDSM.Server/wwwroot/assets/js/product-page.js
@@ -0,0 +1,6 @@
+﻿function updateAbout(value) {
+    let converter = new showdown.Converter()
+    $("#product-page-overview")[0].innerHTML = converter.makeHtml(value);
+    console.log(value)
+}
+updateAbout($("#product-page-overview")[0].innerHTML)
