facfee6802c52627a1b0422cfdc6f618e2bfd6cf
dcmanproductions <dcmanproductions@gmail.com>
2022-07-30 00:22:01 -0400

    added youtube controller

---
+++ OpenDSM.Server/Controllers/API/YTController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenDSM.Core.Handlers;
+
+namespace OpenDSM.Server.Controllers.API;
+
+[Route("/api/yt")]
+public class YTController : ControllerBase
+{
+    [HttpGet("channel/{id}")]
+    public IActionResult GetChannelUploads(string id)
+    {
+        return new JsonResult(YTHandler.GetChannelVideos(id).Result);
+    }
+}
