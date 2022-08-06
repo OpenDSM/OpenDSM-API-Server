e97fa75aeaf68379d637f90e0171333004f09cca
dcmanproductions <dcmanproductions@gmail.com>
2022-08-06 11:17:07 -0400

    Added Database API Controller

---
+++ OpenDSM.Server/Controllers/API/DBController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenDSM.SQL;
+
+namespace OpenDSM.Server.Controllers.API;
+
+[ApiController]
+[Route("/api/db")]
+public class DBController : ControllerBase
+{
+    [HttpGet("status")]
+    public IActionResult Index()
+    {
+        if (Connections.TestConnection(out string version))
+        {
+            return new JsonResult(new
+            {
+                ok = true,
+                version
+            });
+        }
+
+        return BadRequest(new
+        {
+            ok = false,
+        });
+    }
+}
