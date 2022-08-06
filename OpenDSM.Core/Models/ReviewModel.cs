9f86fc5504e9ddda64988b1452291e06adb1891f
dcmanproductions <dcmanproductions@gmail.com>
2022-08-06 11:15:34 -0400

    Added Reviews

---
+++ OpenDSM.Core/Models/ReviewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDSM.Core.Models;
+
+public class ReviewModel
+{
+    public UserModel User { get; set; }
+    public ProductModel Product { get; set; }
+    public string Summery { get; set; }
+    public DateTime Posted { get; set; }
+    public int Rating { get; set; }
+}
