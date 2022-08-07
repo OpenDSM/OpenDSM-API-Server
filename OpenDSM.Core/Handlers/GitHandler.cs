8e8c89e57fd52773cd375e8723edce5b46d453f5
dcmanproductions <dcmanproductions@gmail.com>
2022-08-07 01:41:36 -0400

    Added version fetcher and CreateWebHook function

--- OpenDSM.Core/Handlers/GitHandler.cs
+++ OpenDSM.Core/Handlers/GitHandler.cs
@@ -1,5 +1,8 @@
 ﻿// LFInteractive LLC. (c) 2021-2022 - All Rights Reserved
 using Newtonsoft.Json.Linq;
+using OpenDSM.Core.Models;
+using OpenDSM.SQL;
+using System.Net.Http.Json;
 
 namespace OpenDSM.Core.Handlers;
 
@@ -51,6 +54,87 @@ public static class GitHandler
         return repos.ToArray();
     }
 
+    public static bool GetVersionFromID(string git_repo, int git_version_id, int product_id, GitCredentials credentials, out VersionModel version)
+    {
+        ReleaseType releaseType = ReleaseType.Unkown;
+        string version_name = "";
+        List<PlatformVersion> platforms = new();
+        version = null;
+        if (CheckCredentials(credentials))
+        {
+            using HttpClient client = GetClient(credentials);
+            using HttpResponseMessage response = client.GetAsync($"https://api.github.com/repos/{credentials.Username}/{git_repo}/releases/{git_version_id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+
+                    JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                    if (json != null)
+                    {
+                        version_name = (string)json["name"];
+                        foreach (ReleaseType type in Enum.GetValues(typeof(ReleaseType)))
+                        {
+                            if (version_name.ToLower().Contains(type.ToString().ToLower()))
+                            {
+                                releaseType = type;
+                                break;
+                            }
+                        }
+                        version_name = version_name.Replace(releaseType.ToString(), "").Trim().Trim('-').Trim(); // 'ReleaseType - Version_Name'  -> ' - Version_Name' -> 'Version_Name'
+                        if (json["assets"] != null)
+                        {
+                            JArray assets = (JArray)json["assets"];
+                            foreach (JToken token in assets)
+                            {
+                                JObject asset = (JObject)token;
+                                string name = (string)asset["name"];
+                                foreach (Platform platform in Enum.GetValues(typeof(Platform)))
+                                {
+                                    if (name.ToLower().Contains(platform.ToString().ToLower()))
+                                    {
+                                        if (asset["browser_download_url"] != null)
+                                        {
+                                            platforms.Add(new(platform, (string)asset["browser_download_url"], (int)(asset["download_count"] ?? 0), 0, (long)(asset["size"] ?? 0)));
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            version = new((long)json["id"], product_id, version_name, releaseType, platforms, (string)(json["body"] ?? ""));
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    log.Error($"Unable to GetVersionFromID: {git_version_id}", e.Message, e.StackTrace);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int[] GitReleases(string repo_name, GitCredentials credentials)
+    {
+        List<int> ids = new();
+        if (CheckCredentials(credentials))
+        {
+            using HttpClient client = GetClient(credentials);
+            using HttpResponseMessage response = client.GetAsync($"https://api.github.com/repos/{credentials.Username}/{repo_name}/releases").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                JArray array = JArray.Parse(response.Content.ReadAsStringAsync().Result);
+                foreach (JToken token in array)
+                {
+                    JObject json = (JObject)token;
+                    ids.Add((int)json["id"]);
+                }
+            }
+        }
+        return ids.ToArray();
+    }
+
     public static bool HasReadME(string RepositoryName, GitCredentials credentials, out string ReadMe)
     {
         if (CheckCredentials(credentials))
@@ -85,6 +169,26 @@ public static class GitHandler
         }
         ReadMe = "";
         return false;
+    }
+
+    public static void CreateWebHook(GitCredentials credentials, ProductModel product)
+    {
+        if (CheckCredentials(credentials))
+        {
+            using HttpClient client = GetClient(credentials);
+            using HttpRequestMessage message = new(HttpMethod.Post, $"https://api.github.com/repos/{credentials.Username}/{product.GitRepositoryName}/hooks");
+            message.Content = JsonContent.Create(new
+            {
+                active = true,
+                events = new string[] { "release" },
+                config = new
+                {
+                    url = $"https://opendsm.tk/api/product/trigger-version-check?product_id={product.Id}",
+                    content_type = "application/json"
+                }
+            });
+            using HttpResponseMessage response = client.Send(message);
+        }
     }
 
     #endregion Public Methods
