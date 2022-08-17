67aaf8ccf28e8a06b3f7c3d39c8cbb2f9ac54c16
dcmanproductions <dcmanproductions@gmail.com>
2022-08-17 11:53:55 -0400

    Product will now cache readme, and versions

--- OpenDSM.Core/Models/ProductModel.cs
+++ OpenDSM.Core/Models/ProductModel.cs
@@ -7,6 +7,7 @@ namespace OpenDSM.Core.Models;
 
 public class ProductModel
 {
+
     #region Protected Constructors
 
     protected ProductModel(int id, int userID, string gitRepoName, string name, string summery, bool useGitReademe, string videoKey, uint price, int[] tags, string[] keywords, bool subscription, int pageViews, DateTime posted)
@@ -26,6 +27,7 @@ public class ProductModel
         ShortSummery = summery;
         TotalPageViews = pageViews;
         Posted = posted;
+        PopulateVersionsFromDB();
     }
 
     #endregion Protected Constructors
@@ -36,11 +38,21 @@ public class ProductModel
     {
         get
         {
-            if (UseGitReadME && GitHandler.HasReadME(GitRepositoryName, new(User.GitUsername, User.GitToken), out string readme))
+            string path = Path.Combine(GetProductDirectory(Id), "about.md");
+
+            if (UseGitReadME)
             {
-                return readme;
+                if (!File.Exists(path))
+                {
+                    if (GitHandler.HasReadME(GitRepositoryName, User.GitCredentials, out string readme))
+                    {
+                        About = readme;
+                        return readme;
+                    }
+                }
             }
-            using FileStream fs = new(Path.Combine(GetProductDirectory(Id), "about.md"), FileMode.OpenOrCreate, FileAccess.Read);
+
+            using FileStream fs = new(path, FileMode.OpenOrCreate, FileAccess.Read);
             using StreamReader reader = new(fs);
             return reader.ReadToEnd();
         }
@@ -78,10 +90,8 @@ public class ProductModel
             }
         }
     }
-    public bool Subscription { get; private set; }
     public string GitRepositoryName { get; private set; }
     public bool HasYoutubeVideo => YTHandler.IsValidYoutubeKey(YoutubeKey);
-
     public string IconUrl
     {
         get
@@ -98,20 +108,21 @@ public class ProductModel
     public string[] Keywords { get; set; }
     public string Name { get; private set; }
     public Platform[] Platforms { get; set; }
+    public DateTime Posted { get; set; }
     public uint Price { get; private set; }
-    public int[] Tags { get; set; }
-    public int TotalDownloads { get; private set; }
-    public int TotalWeeklyDownloads { get; private set; }
-    public int TotalPageViews { get; private set; }
-    public string ShortSummery { get; set; }
     public byte Rating { get; private set; }
     public List<ReviewModel> Reviews { get; private set; }
-    public DateTime Posted { get; set; }
-    public float ViewDownloadRatio => TotalDownloads / TotalPageViews;
+    public string ShortSummery { get; set; }
+    public bool Subscription { get; private set; }
+    public int[] Tags { get; set; }
+    public int TotalDownloads { get; private set; }
+    public int TotalPageViews { get; private set; }
+    public int TotalWeeklyDownloads { get; private set; }
     public bool UseGitReadME { get; }
     public UserModel User { get; private set; }
-    public string YoutubeKey { get; private set; }
     public Dictionary<long, VersionModel> Versions { get; private set; }
+    public float ViewDownloadRatio => TotalDownloads / TotalPageViews;
+    public string YoutubeKey { get; private set; }
 
     #endregion Public Properties
 
@@ -121,7 +132,6 @@ public class ProductModel
     {
         try
         {
-
             if (Products.GetProductFromID(id, out string name, out string gitRepoName, out string summery, out bool useGitReadme, out bool subscription, out int[] tags, out string[] keywords, out int price, out string yt_key, out int owner_id, out int pageViews, out DateTime posted))
             {
                 return new(id, owner_id, gitRepoName, name, summery, useGitReadme, yt_key, (uint)price, tags, keywords, subscription, pageViews, posted);
@@ -139,7 +149,8 @@ public class ProductModel
         model = null;
         if (Products.Create(user.Id, gitRepoName, name, yt_key, subscription, use_git_readme, price, keywords, tags, out int product_id) && TryGetByID(product_id, out model))
         {
-            GitHandler.CreateWebHook(new(user.GitUsername, user.GitToken), model);
+            GitHandler.CreateWebHook(user.GitCredentials, model);
+            model.PopulateVersionsFromGit();
             return true;
         }
         return false;
@@ -151,34 +162,9 @@ public class ProductModel
         return model != null;
     }
 
-    public void PopulateVersions()
+    public void AddPageView()
     {
-        TotalDownloads = TotalWeeklyDownloads = 0;
-        List<VersionModel> versions_list = new();
-        List<Platform> platforms = new();
-        try
-        {
-            foreach (int version_id in GitHandler.GitReleases(GitRepositoryName, new(User.GitUsername, User.GitToken)))
-            {
-                if (GitHandler.GetVersionFromID(GitRepositoryName, version_id, Id, new(User.GitUsername, User.GitToken), out VersionModel version))
-                {
-                    versions_list.Add(version);
-                    foreach (PlatformVersion platform in version.Platforms)
-                    {
-                        if (!platforms.Contains(platform.platform))
-                        {
-                            platforms.Add(platform.platform);
-                        }
-                    }
-                    TotalDownloads += version.TotalDownloads;
-                    TotalWeeklyDownloads += version.WeeklyDownloads;
-                }
-            }
-        }
-        catch { }
-        Platforms = platforms.ToArray();
-        Versions = versions_list.ToDictionary(i => i.ID);
-
+        Products.AddPageView(Id);
     }
 
     public void PopulateReviews()
@@ -201,9 +187,96 @@ public class ProductModel
         }
     }
 
-    public void AttemptUniquePageView(IPAddress connecting_address)
+    public void PopulateVersionsFromDB()
     {
+        long[] version_ids = SQL.Versions.GetVersionsByProductID(Id);
+        if (version_ids.Any())
+        {
+            TotalDownloads = TotalWeeklyDownloads = 0;
+            List<VersionModel> versions_list = new();
+            List<Platform> platforms = new();
+            try
+            {
+                foreach (long version_id in version_ids)
+                {
+                    if (SQL.Versions.GetVersionByID(version_id, Id, out string name, out byte releaseTypeB, out byte[] platformsB, out string changelog, out DateTime posted))
+                    {
+                        ReleaseType type = (ReleaseType)releaseTypeB;
+                        List<PlatformVersion> platformVersions = new();
+                        foreach (byte p in platformsB)
+                        {
+                            Platform platform = (Platform)p;
 
+                            if (!platforms.Contains(platform))
+                            {
+                                platforms.Add(platform);
+                            }
+                            if (SQL.Versions.GetPlatformVersionByID(p, version_id, out string download_url, out int total_downloads, out int weekly_downloads, out long filesize))
+                            {
+                                platformVersions.Add(new(platform, download_url, total_downloads, weekly_downloads, filesize));
+                            }
+                        }
+
+                        VersionModel version = new(version_id, Id, name, type, platformVersions, changelog, posted);
+                        versions_list.Add(version);
+                        TotalDownloads += version.TotalDownloads;
+                        TotalWeeklyDownloads += version.WeeklyDownloads;
+                    }
+                }
+            }
+            catch { }
+            Platforms = platforms.ToArray();
+            Versions = versions_list.OrderByDescending(i => i.Posted).ToDictionary(i => i.ID);
+        }
+        else
+        {
+            PopulateVersionsFromGit();
+        }
+    }
+
+    public void PopulateVersionsFromGit()
+    {
+        TotalDownloads = TotalWeeklyDownloads = 0;
+        List<VersionModel> versions_list = new();
+        List<Platform> platforms = new();
+        try
+        {
+            long[] remoteVersions = GitHandler.GitReleases(GitRepositoryName, User.GitCredentials).Result;
+            foreach (long version_id in SQL.Versions.GetVersionsByProductID(Id))
+            {
+                if (!remoteVersions.Contains(version_id))
+                {
+                    SQL.Versions.RemoveVersion(Id, version_id);
+                }
+            }
+            foreach (long version_id in remoteVersions)
+            {
+                if (GitHandler.GetVersionFromID(GitRepositoryName, (int)version_id, Id, User.GitCredentials, out VersionModel version))
+                {
+                    versions_list.Add(version);
+                    foreach (PlatformVersion platform in version.Platforms)
+                    {
+                        if (!platforms.Contains(platform.platform))
+                        {
+                            platforms.Add(platform.platform);
+                        }
+                        if (!SQL.Versions.PlatformExists(version_id, (byte)platform.platform))
+                        {
+                            SQL.Versions.CreatePlatformVersion(platform.platform, platform.downloadUrl, version_id, platform.file_size);
+                        }
+                    }
+                    if (!SQL.Versions.VersionExists(version_id, Id))
+                    {
+                        SQL.Versions.CreateVersion(version_id, Id, version.Name, (byte)version.Type, version.Changelog, version.Posted);
+                    }
+                    TotalDownloads += version.TotalDownloads;
+                    TotalWeeklyDownloads += version.WeeklyDownloads;
+                }
+            }
+        }
+        catch { }
+        Platforms = platforms.ToArray();
+        Versions = versions_list.OrderByDescending(i => i.Posted).ToDictionary(i => i.ID);
     }
 
     #endregion Public Methods
