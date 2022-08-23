a6d69f09a51adeffae2c5206139084a6be8ffc99
dcmanproductions <dcmanproductions@gmail.com>
2022-08-23 13:01:33 -0400

    Added ability to search for users

---
+++ OpenDSM.Core/Handlers/UserListHandler.cs
@@ -0,0 +1,21 @@
+﻿
+using OpenDSM.Core.Models;
+
+namespace OpenDSM.Core.Handlers;
+
+public static class UserListHandler
+{
+    public static UserModel[] GetUserFromPartials(int maxSize, params string[] partials)
+    {
+        List<UserModel> users = new();
+        int[] u = SQL.Authoriztaion.GetUsersWithPartialUsername(maxSize,partials);
+        Parallel.ForEach(u, id =>
+        {
+            UserModel? user = UserModel.GetByID(id);
+            if (user != null)
+                users.Add(user);
+        });
+
+        return users.ToArray();
+    }
+}
