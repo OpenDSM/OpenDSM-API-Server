23f185c61f6a3f4eea8375d7ae3c5f36acacf894
dcmanproductions <dcmanproductions@gmail.com>
2022-09-06 17:18:58 -0400

    added documentation

--- OpenDSM.Server/Controllers/API/VideoController.cs
+++ OpenDSM.Server/Controllers/API/VideoController.cs
@@ -9,12 +9,22 @@ public class VideoController : ControllerBase
 
     #region Public Methods
 
+    /// <summary>
+    /// Returns a list of all youtube videos in a youtube channel
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     [HttpGet("channel/{id}")]
     public IActionResult GetChannelUploads(string id)
     {
         return new JsonResult(YTHandler.GetChannelVideos(id).Result);
     }
 
+    /// <summary>
+    /// Redirects to the direct video url based on the youtube key
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     [HttpGet("{key}")]
     public IActionResult GetVideo(string key)
     {
