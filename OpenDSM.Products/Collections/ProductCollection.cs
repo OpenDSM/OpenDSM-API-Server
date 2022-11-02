e28b1f9dd8d9a923a00348a881970f9dc22186aa
Drew Chase <dcmanproductions@gmail.com>
2022-11-02 14:55:48 -0400

    Added ability to get product based on product_id

---
+++ OpenDSM.Products/Collections/ProductCollection.cs
@@ -0,0 +1,23 @@
+﻿
+using OpenDSM.Products.Models;
+using OpenDSM.Products.SQL;
+
+namespace OpenDSM.Products.Collections;
+
+public static class ProductCollection
+{
+
+    public static ProductModel Get(int product_id)
+    {
+        ProductModel model = LoadedProductsCollection.Instance.Get(product_id);
+        if (model.IsEmpty)
+        {
+            if (ProductDB.TryGetProduct(product_id, out model))
+            {
+                LoadedProductsCollection.Instance.Add(model);
+            }
+        }
+        return model;
+    }
+    public static bool CheckSlugExists(string slug) => ProductDB.CheckIfSlugExists(slug);
+}
